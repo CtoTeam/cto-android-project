c1020b4cf2e397c9bb76f5929309f0b47ce1a636
ctoteam2014@gmail.com <ctoteam2014>
2014-04-30 12:44:47 +0300

    added support v7.. fixing things...

--- CertificateChecker/src/cto/team/certificatechecker/ui/activities/MainActivity.java
+++ CertificateChecker/src/cto/team/certificatechecker/ui/activities/MainActivity.java
@@ -1,45 +1,64 @@
 package cto.team.certificatechecker.ui.activities;
 
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.os.Build;
 
-import com.google.gson.JsonObject;
+public class MainActivity extends ActionBarActivity {
 
-import cto.team.certificatechecker.R;
-import cto.team.certificatechecker.networking.response.ProgressResponseListener;
-import cto.team.certificatechecker.networking.utils.ServerUtils;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-public class MainActivity extends BaseActivity {
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
 
-	private Button button1;
-	private TextView textView1;
-	private ProgressResponseListener progressResponseListener;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-		button1 = (Button) findViewById(R.id.button1);
-		textView1 = (TextView) findViewById(R.id.textView1);
-		progressResponseListener = new ProgressResponseListener(this) {
-			@Override
-			public void onComplete(JsonObject result) {
-				super.onComplete(result);
-				textView1.setText(result.toString());
-			}
-		};
-		button1.setOnClickListener(new OnClickListener() {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-			@Override
-			public void onClick(View v) {
-				progressResponseListener.show();
-				ServerUtils.runGetRequest("http://api.openweathermap.org/data/2.5/weather?q=London,uk", null, progressResponseListener);
-			}
-		});
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
 
-	}
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
 }
