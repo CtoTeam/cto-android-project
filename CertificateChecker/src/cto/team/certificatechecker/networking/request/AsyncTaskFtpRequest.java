f3a3ed5a56836ca126347a68a4be26f22f05f4d3
Gal Madar <madargal@gmail.com>
2014-05-01 14:52:57 +0300

    added the AsyncTaskFtpRequest

---
+++ CertificateChecker/src/cto/team/certificatechecker/networking/request/AsyncTaskFtpRequest.java
@@ -0,0 +1,51 @@
+package cto.team.certificatechecker.networking.request;
+
+import java.io.InputStream;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+public class AsyncTaskFtpRequest extends AsyncTask<String, String, Bitmap> {
+	
+	private FTPClient ftpClient;
+	private ImageView imageView;
+	
+	public AsyncTaskFtpRequest(ImageView imageView) {
+		this.ftpClient = new FTPClient();
+		this.imageView = imageView;
+	}
+	
+	@Override
+	protected Bitmap doInBackground(String... params) {
+		try {
+		ftpClient.connect("ftp.podserver.info");
+		if (FTPReply.isPositiveCompletion(ftpClient.getReplyCode())) {
+	           boolean status = ftpClient.login("podi_14742696", "LeadLead");
+	           ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+	           ftpClient.enterLocalPassiveMode();
+	           ftpClient.changeWorkingDirectory("/htdocs");
+
+	           InputStream imageInputStream = ftpClient.retrieveFileStream("2.png");
+	           
+	           Bitmap image = BitmapFactory.decodeStream(imageInputStream);
+	           
+	           return image;
+	        }
+		
+		return null;
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+	}
+	
+	protected void onPostExecute(Bitmap result) {
+		imageView.setImageBitmap(result);
+	};
+}
